f488841f389bf761fbc4c5f19f3e16654972c121
Lily <1502860865@qq.com>
2017-02-21 20:54:17 +0800

    添加数据库和表

---
+++ app/src/main/java/weather/com/cool/coolweather/db/County.java
@@ -0,0 +1,43 @@
+package weather.com.cool.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private int countyCode;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(int countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
